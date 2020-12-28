8ce10c1ea978c5e7a125c147f68536e63adbbe07
Peter Zhang <unoinoit@gmail.com>
2020-12-27 23:12:09 -0500

    Missing changes for CME settle

--- controller/controller_data_scraper.py
+++ controller/controller_data_scraper.py
@@ -1,17 +1,20 @@
-import os, sys
+import os, sys, datetime
 
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 import data_scraper.ds_cme
+import tools.utilities
 
-def run_data_scraper():
-    cme_scraper = data_scraper.ds_cme.DataScraperCME()
-    cme_scraper.scrape_data()
+def run_data_scraper(arg_logger, arg_data_dir):
+    cme_scraper = data_scraper.ds_cme.DataScraperCME(arg_logger, arg_data_dir)
+    cme_scraper.scrape_data(datetime.date(2020, 12, 24))
 
 def main():
-    run_data_scraper()
+    t_logger = tools.utilities.LogHandler('controller_ds', '//NAS408704/Public/Documents/Work/Zhiyong/Log')
+    t_data_dir = '//NAS408704/Public/Documents/Work/Zhiyong/Data'
+    run_data_scraper(t_logger, t_data_dir)
 
 if __name__ == "__main__":
     main()
--- data_scraper/ds_cme.py
+++ data_scraper/ds_cme.py
@@ -1,5 +1,6 @@
 import os, datetime
 import ftplib
+import re
 from enum import Enum
 
 class CMEFileCategory(Enum):
@@ -7,22 +8,46 @@ class CMEFileCategory(Enum):
     SPAN = 2
 
 class DataScraperCME(object):
-    def __init__(self, arg_logger):
+    def __init__(self, arg_logger, arg_data_dir):
         self._logger = arg_logger
+        self._data_dir = arg_data_dir
         self._url = 'ftp.cmegroup.com'
-        self._ftp_path_settle = 'settle'
-        self._ftp_path_span = 'pub/span/data'
         self._ftp_handle = None
 
-    def scrape_data(self, arg_date: datetime.date = datetime.date.today()) -> None:
-        t_file_name_settle = F'settle.{arg_date.strftime("%Y%m%d")}.s.xml.zip'
+        self._dict_file_category = {}
 
-        self._connect_ftp()
-        self._download_file_from_ftp(self._ftp_path_settle, t_file_name_settle)
+        self._dict_file_category[CMEFileCategory.Settle] = {}
+        self._dict_file_category[CMEFileCategory.Settle]['ftp_path'] = 'settle'
+        self._dict_file_category[CMEFileCategory.Settle]['data_dir'] = os.path.join(self._data_dir, 'cme_settle')
+        self._dict_file_category[CMEFileCategory.Settle]['file_name_format'] = 'settle.{date}.s.xml.zip'
 
-    def _connect_ftp(self) -> None:
-        self._ftp_handle = ftplib.FTP(self._url)
-        self._ftp_handle.login()
+            # self._ftp_path_span = 'pub/span/data'
+
+    def scrape_data(self, arg_date: datetime.date = datetime.date.today()) -> bool:
+        if not self._connect_ftp():
+            return False
+
+        for t_file_type in self._dict_file_category:
+            try:
+                t_data_dir = self._dict_file_category[t_file_type]['data_dir']
+                if not os.path.exists(t_data_dir):
+                    os.makedirs(t_data_dir)
+            except OSError as e:
+                self._logger.log_error(F'Create data folder ({t_data_dir}) failed: {e}')
+                return False
+
+            t_file_name = re.sub('{date}', F'{arg_date:%Y%m%d}', self._dict_file_category[t_file_type]['file_name_format'])
+            self._download_file_from_ftp(self._dict_file_category[t_file_type]['ftp_path'], t_file_name, t_data_dir)
+
+    def _connect_ftp(self) -> bool:
+        try:
+            self._ftp_handle = ftplib.FTP(self._url)
+            self._ftp_handle.login()
+        except ftplib.all_errors as e:
+            self._logger.log_error(F'{self._url} login failed: {e}')
+            return False
+
+        return True
 
     def _download_file_from_ftp(self, arg_ftp_path: str, arg_file_name: str, arg_local_path: str) -> None:
         # Set ftp handle to root directory
@@ -31,20 +56,22 @@ class DataScraperCME(object):
         # Verify arg_ftp_path
         t_path_folders = arg_ftp_path.split('/')
         for t_path_folder in t_path_folders:
-            t_listed_folders = self._ftp_handle.mlsd()
-            if t_path_folders not in t_listed_folders:
+            t_listed_items = self._ftp_handle.nlst()
+            if t_path_folder not in t_listed_items:
                 self._logger.log_error(F'Cannot find FTP path {arg_ftp_path}! Download failed.')
                 break
             self._ftp_handle.cwd(t_path_folder)
 
         # Download files with arg_file_name in file name
-        t_listed_files = self._ftp_handle.mlsd()
-        for t_file_name in t_listed_files:
-            if arg_file_name in t_file_name:
-                t_local_full_path = os.path.join(arg_local_path, t_file_name)
-                with open(t_local_full_path, 'w') as t_local_file_handle:
-                    t_result = self._ftp_handle.retrbinary(F'RETR {t_file_name}', t_local_file_handle.write)
+        t_listed_items = self._ftp_handle.nlst()
+        for t_item_name in t_listed_items:
+            if arg_file_name in t_item_name:
+                t_local_full_path = os.path.join(arg_local_path, t_item_name)
+                with open(t_local_full_path, 'wb') as t_local_file_handle:
+                    t_result = self._ftp_handle.retrbinary(F'RETR {t_item_name}', t_local_file_handle.write)
                     if not t_result.startswith('226 Transfer complete'):
-                        self._logger.log_error(F'Download {t_file_name} failed.')
+                        self._logger.log_error(F'Download {t_item_name} failed.')
+                    else:
+                        self._logger.log_message(F'Download is completed: {t_local_full_path}')
             
             
