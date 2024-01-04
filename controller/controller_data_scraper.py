99ebf06a1afb1a1fa32641b404d48ca5b2976b92
Peter Zhang <unoinoit@gmail.com>
2024-01-03 22:38:38 -0600

    fix cme ftp download

--- controller/controller_data_scraper.py
+++ controller/controller_data_scraper.py
@@ -10,12 +10,14 @@ import tools.utilities
 def run_data_scraper(arg_logger, arg_data_dir):
     cme_scraper = data_scraper.ds_cme.DataScraperCME(arg_logger, arg_data_dir)
     t_today = datetime.date.today()
-    arg_logger.log_message(F'To download CME files for {t_today: %Y-%m-%d}')
-    cme_scraper.scrape_data(datetime.date(t_today.year, t_today.month, t_today.day))
+    for i in range(7):
+        arg_logger.log_message(F'To download CME files for {t_today: %Y-%m-%d}')
+        cme_scraper.scrape_data(datetime.date(t_today.year, t_today.month, t_today.day))
+        t_today += datetime.timedelta(days=-1)
 
 def main():
-    t_logger = tools.utilities.LogHandler('controller_ds', '/mnt/nas_public/Documents/Work/Zhiyong/Log')
-    t_data_dir = '/mnt/nas_public/Documents/Work/Zhiyong/Data'
+    t_logger = tools.utilities.LogHandler('controller_ds', '~/Documents/projects/Log')
+    t_data_dir = '~/Documents/projects/Data'
     run_data_scraper(t_logger, t_data_dir)
 
 if __name__ == "__main__":
